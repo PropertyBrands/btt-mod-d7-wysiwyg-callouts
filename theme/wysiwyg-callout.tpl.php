998de39d6f5e99be1f58ab436bb1a3430d2bc8b3
ethanhinson <ethan@bluetent.com>
2018-05-22 23:36:08 -0600

    Pass 1 of module implementation

---
+++ theme/wysiwyg-callout.tpl.php
@@ -0,0 +1,8 @@
+<div class="wysiwyg-callout wysiwyg-callout-<?php print $image_side; ?>">
+  <div class="image">
+    <img alt="<?php print $copy; ?>" src="<?php print $image; ?>" />
+  </div>
+  <div class="copy">
+    <?php print $copy; ?>
+  </div>
+</div>
