7cd7e1636575e042da021979db0da03480f371c3
ethanhinson <ethan@bluetent.com>
2018-06-05 11:39:44 -0600

    Hardcode wysiwyg callouts

--- theme/wysiwyg-callout.tpl.php
+++ theme/wysiwyg-callout.tpl.php
@@ -1,8 +1,17 @@
-<div class="wysiwyg-callout wysiwyg-callout-<?php print $image_side; ?>">
+<div class="wysiwyg-callout wysiwyg-callout-right">
   <div class="image">
-    <img alt="<?php print $copy; ?>" src="<?php print $image; ?>" />
+    <img alt="High-level exposure to promotions on 25+ sites including VRBO, Homeaway, and AirBnB" src="https://www.walkervr.com/sites/default/files/uploads/right-distribution_1.png">
   </div>
   <div class="copy">
-    <?php print $copy; ?>
+    <h5>High-level exposure to promotions on 25+ sites including VRBO, Homeaway, and AirBnB</h5>
+    <p>Your listing will be optimized and distributed to over 25+ vacation rental websites. as well as running targeted paid advertising campaign. This distribution process will put you in front of more than 90% of the online travel searchers.</p>  </div>
+</div>
+
+<div class="wysiwyg-callout wysiwyg-callout-left">
+  <div class="image">
+    <img alt="High-level exposure to promotions on 25+ sites including VRBO, Homeaway, and AirBnB" src="https://www.walkervr.com/sites/default/files/uploads/left-matterport-505x627-smaller.gif">
   </div>
+  <div class="copy">
+    <h5>Some Test</h5>
+    <p>As well as running targeted paid advertising campaign. This distribution process will put you in front of more than 90% of the online travel searchers.</p>  </div>
 </div>
